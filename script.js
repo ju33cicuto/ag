bad884cb024e5f3d2bcd3d8371d174c9350281c0
ju33cicuto <143412201+ju33cicuto@users.noreply.github.com>
2023-10-06 16:29:30 -0300

    Update script.js- eliminação

--- script.js
+++ script.js
@@ -32,21 +32,21 @@ const contactsList = [
         spanType.classList.add('phone');
     }
 
-    // Criar o elemento svg com o ícone de lixeira
+
     const trashIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     trashIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
     trashIcon.setAttribute('viewBox', '0 0 448 512');
 
-    // Adicionar o path do ícone da lixeira ao svg
+
     const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.setAttribute('d', 'M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z');
 
-    // Definir atributos de estilo para o ícone
+  
     trashIcon.setAttribute('width', '32');
     trashIcon.setAttribute('height', '32');
-    trashIcon.style.fill = 'currentColor'; // Preenchimento da cor atual (pode ser personalizado)
+    trashIcon.style.fill = 'currentColor';
 
-    // Anexar o path ao svg e o svg ao botão
+  
     trashIcon.appendChild(path);
     btnRemove.appendChild(trashIcon);
 
